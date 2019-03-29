b6fd413cf47d145a161cc749538f80cf345e7213
Talha Kothari <t3k4bb@hotmail.com>
2019-03-29 22:38:20 +0000

    Fixed Bugs

--- my-app.py
+++ my-app.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify, json
 from cassandra.cluster import Cluster
 import requests
 import sys
+import uuid
 
 cluster = Cluster(['cassandra'])
+#cluster = Cluster(['127.0.0.1'])
 session = cluster.connect()
 
 app = Flask(__name__)
@@ -22,42 +24,42 @@ def get_all_music():
     print(row.id,row.name,row.album, file=sys.stderr)
     music = {'id': row.id, 'name': row.name, 'album': row.album}
     music_data.append(music)
-  return jsonify(music_data)
+  return jsonify(music_data), 200
 
 
-@app.route('/music/<int:id>', methods=['GET'])
+@app.route('/music/<string:id>', methods=['GET'])
 def get_artist_by_id(id):
   print('inside get_artist_by_id')
+  print(uuid.UUID(id))
   music_data=[]
-  rows = session.execute("""SELECT * FROM music.artist WHERE id=%(id)s""",{'id': id})
+  rows = session.execute("""SELECT * FROM music.artist WHERE id=%(id)s""",{'id': uuid.UUID(id)})
   for row in rows:
-    print(row.id,row.name,row.original,row.temperature,row.description, file=sys.stderr)
     music = {'id': row.id, 'name': row.name, 'album': row.album}
     music_data.append(music)
-  return jsonify(music_data)
+  return jsonify(music_data), 200
 
 
 @app.route('/music', methods=['POST'])
 def create_artist():
-        print('inside create_artist')
-        rows = session.execute("INSERT INTO music.artist (id, name, album) VALUES (%s, %s, %s)", (1, request.form['name'], request.form['album']))
-        return jsonify({'message':'new record created'})
+  print('inside create_artist')
+  rows = session.execute("INSERT INTO music.artist (id, name, album) VALUES (%s, %s, %s)", (uuid.uuid1(), request.form['name'], request.form['album']))
+  return jsonify({'message':'new record created'}), 201
 
 
-@app.route('/music/<int:id>', methods = ['PUT'])
+@app.route('/music/<string:id>', methods = ['PUT'])
 def update_artist(id):
   print('inside update_artist')
-  rows = session.execute("""UPDATE music.artist SET name=%(name)s, album=%(album)s WHERE id=%(id)s""", {'name': request.form['name'], 'album': request.form['album'] 'id': id})
+  rows = session.execute("""UPDATE music.artist SET name=%(name)s, album=%(album)s WHERE id=%(id)s""", {'name': request.form['name'], 'album': request.form['album'], 'id': uuid.UUID(id)})
   print(rows,file=sys.stderr)
 
-  return jsonify({'message':'updated successfully'})
+  return jsonify({'message':'updated successfully'}), 200
 
 
-@app.route('/music/<int:id>', methods = ['DELETE'])
+@app.route('/music/<string:id>', methods = ['DELETE'])
 def delete_artist(id):
   print('inside delete_artist')
-  session.execute("DELETE FROM music.artist WHERE id=(%s)", (id))
-  return jsonify({'message':'delete successfull'})
+  session.execute("""DELETE FROM music.artist WHERE id=%(id)s""", { 'id': uuid.UUID(id)})
+  return jsonify({'message':'delete successfull'}), 200
 
 
 if __name__ == '__main__':
